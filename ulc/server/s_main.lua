dd539abe2146f8b8eb7a80715d77bd0efe57faae
Flohhhhh <camgus322@gmail.com>
2023-03-04 17:03:44 -0500

    Update s_main.lua

--- ulc/server/s_main.lua
+++ ulc/server/s_main.lua
@@ -126,6 +126,27 @@ local function CheckData(data, resourceName)
     TriggerEvent("ulc:warn", 'A config in "' .. resourceName .. '" uses Horn Extras, but no horn extras were specified.')
   end
 
+  if data.defaultStages.useDefaults then
+    if #data.enableKeys == 0 then
+      TriggerEvent("ulc:warn", 'A config in "'.. resourceName .. '" uses Default Stages, but no keys were specified to enable (enableKeys = {}).')
+    else
+      for _, v in pairs(data.defaultStages.enableKeys) do
+        if v > 9 then
+          TriggerEvent("ulc:error", 'A config in "'.. resourceName .. '" has an invalid key in enableKeys = {}. Value must be 1-9 representing numpad keys.')
+        end
+      end
+    end
+    if #data.disableKeys == 0 then
+      TriggerEvent("ulc:warn", 'A config in "'.. resourceName .. '" uses Default Stages, but no keys were specified to disable (disableKeys = {}).')
+    else
+      for _, v in pairs(data.defaultStages.disableKeys) do
+        if v > 9 then
+          TriggerEvent("ulc:error", 'A config in "'.. resourceName .. '" has an invalid key in disableKeys = {}. Value must be 1-9 representing numpad keys.')
+        end
+      end
+    end
+  end
+
   -- Buttons
   -- check if vehicle uses buttons but hud is disabled
   if #data.buttons > 0 and Config.hideHud == true then
