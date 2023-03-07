4bee1b702d84cf6fa09667608a6eaeb38c6c6093
Flohhhhh <48927090+Flohhhhh@users.noreply.github.com>
2023-03-07 13:33:03 -0500

    Update fxmanifest.lua

--- ulc/fxmanifest.lua
+++ ulc/fxmanifest.lua
@@ -7,7 +7,7 @@ description "The ultimate non-els lighting controller. Documentation: https://do
 author "Dawnstar"
 version "1.5.0"
 
-ui_page "ui/index.html"
+ui_page "ui/lights.html"
 
 files {
 	"ui/lights.html",
