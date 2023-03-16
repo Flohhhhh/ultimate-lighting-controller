e02441dd10675fc4ff29b3530672336b7187a968
Flohhhhh <camgus322@gmail.com>
2023-03-16 14:05:21 -0400

    Doors

---
+++ ulc/client/c_doors.lua
@@ -0,0 +1,62 @@
+local doors = {
+    [0] = false, -- d front
+    [1] = false, -- p front
+    [2] = false, -- d rear
+    [3] = false, -- p rear
+    [4] = false, -- hood
+    [5] = false -- trunk
+}
+
+-- state 1 = closed, state 0 = open
+local function onDoorStateChange(door, newDoorState)
+    if door == 0 or door == 2 then -- if driver side
+        for _, v in pairs(MyVehicleConfig.doorConfig.driverSide.enable) do
+            ULC:SetStage(v, newDoorState, true, true, false)
+        end
+        for _, v in pairs(MyVehicleConfig.doorConfig.driverSide.disable) do
+            ULC:SetStage(v, not newDoorState, true, true, false)
+        end
+    elseif door == not newDoorState or door == 3 then -- if pass side
+        for _, v in pairs(MyVehicleConfig.doorConfig.passSide.enable) do
+            ULC:SetStage(v, newDoorState, true, true, false)
+        end
+        for _, v in pairs(MyVehicleConfig.doorConfig.passSide.disable) do
+            ULC:SetStage(v, not newDoorState, true, true, false)
+        end
+    elseif door == 5 then -- if trunk
+        for _, v in pairs(MyVehicleConfig.doorConfig.trunk.enable) do
+            ULC:SetStage(v, newDoorState, true, true, false)
+        end
+        for _, v in pairs(MyVehicleConfig.doorConfig.trunk.disable) do
+            ULC:SetStage(v, not newDoorState, true, true, false)
+        end
+    end
+end
+
+CreateThread(function()
+    local sleep = 1000
+    while true do Wait(sleep)
+        if not MyVehicle then sleep = 1000 goto continue end
+        if not MyVehicleConfig.doorConfig or false then print("Not included") sleep = 1000 goto continue end
+        if not MyVehicleConfig.doorConfig.useDoors then sleep = 1000 goto continue end
+        sleep = 250
+
+        for k, v in pairs(doors) do
+            if GetVehicleDoorAngleRatio(MyVehicle, k) > 0.0 then
+                if v == false then
+                    print("Setting door", k, "open.")
+                    doors[k] = true -- set door open
+                    onDoorStateChange(k, 0) -- handle what to do
+                end
+            else
+                if v == true then
+                    print("Setting door", k, "closed.")
+                    doors[k] = false -- set door closed
+                    onDoorStateChange(k, 1) -- handle what to do
+                end
+            end
+        end
+
+        ::continue::
+    end
+end)
--- ulc/fxmanifest.lua
+++ ulc/fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 name "Ultimate Lighting Controls"
 description "The ultimate non-els lighting controller. Documentation: https://docs.dwnstr.com/ulc/overview"
 author "Dawnstar"
-version "1.6.0"
+version "1.6.1"
 
 ui_page "html/index.html"
 
@@ -34,6 +34,7 @@ client_scripts {
 	'client/c_cruise.lua',
 	'client/c_horn.lua',
 	'client/c_park.lua',
+	'client/c_doors.lua',
 	'client/c_reverse.lua',
 	'client/c_beeps.lua'
 }
