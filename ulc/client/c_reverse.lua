a7d4c581bd2858434b60b4c71f42eb38d5056358
Flohhhhh <48927090+Flohhhhh@users.noreply.github.com>
2023-11-24 17:12:35 -0500

    Fix #17

--- ulc/client/c_reverse.lua
+++ ulc/client/c_reverse.lua
@@ -3,10 +3,14 @@
 local reversing = false
 
 AddEventHandler('ulc:StartCheckingReverseState', function()
-
-    if not MyVehicleConfig.reverseConfig then return end
     CreateThread(function()
-        while MyVehicle do 
+        while true do Wait(250)
+            print("Checking reverse state")
+            if not IsPedInAnyVehicle(PlayerPedId()) then return end
+            -- this feels unncessary, but I think some people may not have .reverseConfig
+            if not MyVehicle then return end
+            if not MyVehicleConfig.reverseConfig then return end
+            if not MyVehicleConfig.reverseConfig.useReverse then return end
             local gear = GetVehicleCurrentGear(MyVehicle)
             if gear == 0 then
                 if not reversing then
@@ -23,7 +27,6 @@ AddEventHandler('ulc:StartCheckingReverseState', function()
                     end
                 end
             end
-            Wait(250)
         end
     end)
 end)
